792f863ae75bc6a2f44447caa802ff320b3cba13
Graeme O <graeme.ong>
2017-12-06 21:31:32 +0800

    [GraemeO] Add basic logger class along with different log levels logging

---
+++ basic_logger.py
@@ -0,0 +1,15 @@
+import logging
+
+class BasicLogger:
+   def __init__(self):
+      logger = logging.getLogger(__name__)
+
+      print "##############################################"
+
+      logger.critical("Mission CRITICAL!!!! Time to panic!!")
+      logger.error("Crap happens!")
+      logger.warning("This is just a warning!! Take it easy...")
+      logger.info("Hello world!")
+      logger.debug("Well, everything you should know is now...")
+
+      print "##############################################"
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import logging
+from basic_logger import BasicLogger
 
 def main():
-   logging.basicConfig(level="NOTSET", format="%(levelname)s %(asctime)s %(name)s %(message)s")
-   logger = logging.getLogger(__name__)
-   logger.info("hello")
-   logger.debug("hello")
-   logger.warning("hello")
+   log_format = "%(levelname)s %(asctime)s %(name)s %(message)s"
+
+   logging.basicConfig(level="NOTSET", format=log_format)
+
+   BasicLogger()
 
 if __name__ == '__main__':
    main()
