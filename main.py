0bc9e4400e5a6b24409557c58cedc279fb2148c7
Graeme O <graeme.ong>
2017-12-06 21:19:57 +0800

    [GraemeO] Add basic log config with log level and format defined

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import logging
 
 def main():
-   print "hello"
+   logging.basicConfig(level="NOTSET", format="%(levelname)s %(asctime)s %(name)s %(message)s")
+   logger = logging.getLogger(__name__)
+   logger.info("hello")
+   logger.debug("hello")
+   logger.warning("hello")
 
 if __name__ == '__main__':
    main()
