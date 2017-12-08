61b8a24693b97f042749715e4c90647a8e96a081
Graeme O <graeme.ong>
2017-12-08 12:40:08 +0800

    [GraemeO] Add arg parse to take in loglevel as an argument when calling the script. README has also been updated

---
+++ README.md
@@ -0,0 +1,20 @@
+# Python Logging with Basic Configuration
+
+## Description
+
+Simple demonstration on how to log in Python using basic configuration.
+
+This sample takes in a log level as an argument to demonstrate various log level output to the console.
+
+
+## Usage(s)
+
+To log, simple execute the following:
+
+```bash
+python main.py --loglevel <LOGLEVEL>
+```
+
+Where:
+
+LOGLEVEL can either be critical, warning, error, info, debug or notset
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import logging
+import argparse
 from basic_logger import BasicLogger
 
 def main():
-   log_format = "%(levelname)s %(asctime)s %(name)s %(message)s"
-   log_level = "INFO"
+   parser = argparse.ArgumentParser(description="")
+   parser.add_argument("-l", "--loglevel", default="info", required=True, help="Log level setting - can either be notset, debug, critical, warning, error or info")
+   args = parser.parse_args()
 
-   logging.basicConfig(level=log_level, format=log_format, filename="")
+   log_format = "%(levelname)s %(asctime)s %(name)s %(message)s"
+   log_level = args.loglevel.upper()
+
+   logging.basicConfig(level=log_level, format=log_format)
 
    basic_logger = BasicLogger()
    basic_logger.log_me_up()
